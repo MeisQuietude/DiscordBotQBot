caf76ee76f4ca71aba3cdff6c55622b1d880ea21
Stepan Savelyev <mail@s-savelyev.ru>
2020-06-07 03:28:05 +0500

    [ADD] WrapText util module

--- utils/index.js
+++ utils/index.js
@@ -1,8 +1,10 @@
 const getRandomNumber = require("./modules/getRandomNumber");
 const Logger = require("./modules/logger");
 // const RainbowRole = require('./modules/rainbow');  MEMORY LEAK
+const { wrapAsQuote } = require("./modules/wrapText");
 
 module.exports = {
     Logger,
     getRandomNumber,
+    wrapAsQuote,
 };
